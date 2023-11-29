b786ef4a746ae9e350c5523221df8265c54b1fa0
anna <ee@ss.e>
2023-11-29 15:58:44 -0800

    task1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-class Average:    def __init__(self):        pass    @staticmethod    def average_value(arr: list) -> int:        """        Среднее значения списка        """        if not arr:            return 0        return sum(arr) / len(arr)    def comparison_numbers(self, arr1, arr2):        """        Функция сравнение средних значений из двух списков        """        array1 = self.average_value(arr1)        array2 = self.average_value(arr2)        if array1 > array2:            return "Первый список имеет большее среднее значение"        if array2 > array1:            return "Второй список имеет большее среднее значение"        return "Средние значения равны"
+class Average:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def average_value(arr: list) -> int:
+        """
+        Среднее значения списка
+        """
+        if not arr:
+            return 0
+        return sum(arr) / len(arr)
+    def comparison_numbers(self, arr1, arr2):
+        """
+        Функция сравнение средних значений из двух списков
+        """
+        array1 = self.average_value(arr1)
+        array2 = self.average_value(arr2)
+        if array1 > array2:
+            return "Первый список имеет большее среднее значение"
+        if array2 > array1:
+            return "Второй список имеет большее среднее значение"
+        return "Средние значения равны"
